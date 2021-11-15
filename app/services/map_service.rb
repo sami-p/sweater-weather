9ff579ca9a91804d67bfbcf02c9dadf468481a76
sami-p <sami.peterson14@gmail.com>
2021-11-15 09:01:06 -0700

    Create map service

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -2,4 +2,12 @@ class MapService
   def self.get_data(url)
     Faraday.new(url)
   end
+
+  def self.location(location)
+    response = get_data("https://www.mapquestapi.com/geocoding/v1/address").get do |f|
+      f.params['key'] = ENV['maps_api']
+      f.params['location'] = location
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
