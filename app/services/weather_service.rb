86fc13585f3432b92ba6a576261ce381157a2dc7
sami-p <sami.peterson14@gmail.com>
2021-11-15 09:01:19 -0700

    Create weather service

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -2,4 +2,15 @@ class WeatherService
   def self.get_data(url)
     Faraday.new(url)
   end
+
+  def self.location(latitude, longitude)
+    response = get_data("https://api.openweathermap.org/data/2.5/onecall").get do |f|
+      f.params['appid'] = ENV['weather_api']
+      f.params['lat'] = latitude
+      f.params['lon'] = longitude
+      f.params['exclude'] = 'minutely,alerts'
+      f.params['units'] = 'imperial'
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
