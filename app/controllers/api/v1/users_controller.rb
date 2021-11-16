534827d30926efc64c2e4980f10c155a80d2e4fe
sami-p <sami.peterson14@gmail.com>
2021-11-16 16:51:38 -0700

    Add conditional to index action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,12 @@
-class Api::V1::UsersController
+class Api::V1::UsersController < ApplicationController
   def create
-    
+    user = User.new(user_params)
+    if user.save
+      user.assign_api_key
+      render json: UsersSerializer.user(user), status: 201
+    else
+      render :json => { :errors => 'Invalid email or password' }, :status => 409
+    end
   end
 
   private
