175cd4e0fe7fd15c804d634e5e299efba4cf95ff
sami-p <sami.peterson14@gmail.com>
2021-11-17 00:44:34 -0700

    Add error handling for no api key

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,9 +2,11 @@ class Api::V1::RoadTripController < ApplicationController
 
   def create
     user = User.find_by(api_key: params[:api_key])
-    if user && params[:origin].present? && params[:destination].present?
+    if user && params[:origin].present? && params[:destination].present? && !params[:api_key].nil?
       road_trip = RoadTripFacade.itinerary(params[:origin], params[:destination])
       render json: RoadTripSerializer.road_trip(road_trip), status: 200
+    else
+      render :json => { :errors => 'Unauthorized' }, :status => 401
     end
   end
 end
