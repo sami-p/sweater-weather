ed18b57779644bdc737884cf7994ef731788ebee
sami-p <sami.peterson14@gmail.com>
2021-11-16 21:19:36 -0700

    Add beginning of conditional in create action

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::RoadTripController < ApplicationController
 
   def create
+    user = User.find_by(api_key: params[:api_key])
+    if user && params[:origin].present? && params[:destination].present?
+      road_trip = RoadTripFacade.itinerary(params[:origin], params[:destination])
+      render json: RoadTripSerializer.road_trip(road_trip), status: 200
+    end
   end
 end
