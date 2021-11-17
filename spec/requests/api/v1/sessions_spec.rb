1b6f35bcd7311d8c8eb563cb9888038751338e95
sami-p <sami.peterson14@gmail.com>
2021-11-17 00:28:33 -0700

    Add sad path testing

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -20,4 +20,38 @@ RSpec.describe 'Sessions Request' do
     expect(results[:attributes][:email]).to eq(user.email)
     expect(results[:attributes][:api_key]).to eq(user.api_key)
   end
+
+  it 'renders an error if password is invalid' do
+    user = User.create!({email: 'shessolucky@shesastar.com',
+                         password: 'britneyspears',
+                         password_confirmation: 'britneyspears',
+                         api_key: 'alsidjfioashefoij84'
+                       })
+
+    body = {email: 'shessolucky@shesastar.com',
+            password: 'britney'}
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept": 'application/json' }
+
+    post '/api/v1/sessions', headers: headers, params: body, as: :json
+
+    results = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(response.status).to eq(401)
+  end
+
+  it 'renders an error if email is invalid' do
+    user = User.create!({email: 'shessolucky@shesastar.com',
+                         password: 'britneyspears',
+                         password_confirmation: 'britneyspears',
+                         api_key: 'alsidjfioashefoij84'
+                       })
+
+    body = {email: 'britney@spears.com',
+            password: 'britneyspears'}
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept": 'application/json' }
+
+    post '/api/v1/sessions', headers: headers, params: body, as: :json
+
+    results = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(response.status).to eq(401)
+  end
 end
