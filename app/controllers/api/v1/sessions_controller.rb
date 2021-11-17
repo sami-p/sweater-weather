9eb2d362475ff74e196e400c18dc1ae46e41d889
sami-p <sami.peterson14@gmail.com>
2021-11-16 17:31:46 -0700

    Create sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      render json: SessionsSerializer.session(user), status: 200
+    else
+      render :json => { :errors => "Invalid input" }, :status => 401
+    end
+  end
+end
