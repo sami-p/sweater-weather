1a3127d195908504a17259c51c857563a1c0b0c3
sami-p <sami.peterson14@gmail.com>
2021-11-17 00:44:47 -0700

    Add api key sad path testing

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Road Trip Api' do
-  it 'can plan road trip with origin, destination, travel time and forecast' do
+  it 'can plan road trip with origin, destination, travel time and forecast', :vcr do
     user = User.create!({email: 'shessolucky@shesastar.com',
                          password: 'britneyspears',
                          password_confirmation: 'britneyspears',
@@ -18,5 +18,19 @@ RSpec.describe 'Road Trip Api' do
 
     results = JSON.parse(response.body, symbolize_names: true)[:data]
     expect(results).to be_a Hash
+  end
+
+  it 'renders error if api key is invalid or does not exist' do
+    user = User.create!({email: 'shessolucky@shesastar.com',
+                         password: 'britneyspears',
+                         password_confirmation: 'britneyspears'
+                        })
+    body = ({origin: 'los angeles,ca',
+             destination: 'new york,ny'})
+
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept": 'application/json' }
+    post '/api/v1/road_trip', headers: headers, params: body, as: :json
+
+    expect(response.status).to eq(401)
   end
 end
