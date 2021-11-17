ba32fb6cae33d3c175f130f49823569a9ac3d888
sami-p <sami.peterson14@gmail.com>
2021-11-16 22:10:54 -0700

    Test generation of api  key

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,19 +4,19 @@ describe User, type: :model do
   describe "validations" do
     it {should validate_presence_of(:email)}
     it {should validate_uniqueness_of(:email)}
-    it {should validate_presence_of(:password_digest)}
-    it {should validate_presence_of(:api_key)}
     it {should validate_uniqueness_of(:api_key)}
   end
 
-  describe "private methods" do
+  describe "methods" do
     describe "#assign_api_key" do
       it 'generates and assigns and random api key for each user' do
         user = User.new({email: 'oopsididitagain@gmail.com',
                         password: 'britneyspears',
                         password_confirmation: 'britneyspears'})
 
-        expect(user.api_key).to eq(24)
+        user.assign_api_key
+
+        expect(user.api_key).to eq(user.api_key)
       end
     end
   end
