0fe80bbb9cf432deb4bbc244bc936b7a8fd976ba
sami-p <sami.peterson14@gmail.com>
2021-11-16 22:15:05 -0700

    Testing final branch

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecast#index'
+      get '/activities', to: 'activities#index'
       get '/backgrounds', to: 'background#index'
       post '/users', to: 'users#create'
       post '/sessions', to: 'sessions#create'
--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -1,25 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Road Trip Api' do
-  before :each do
-    @user = User.create!({email: 'shessolucky@shesastar.com',
+  it 'can plan road trip with origin, destination, travel time and forecast' do
+    user = User.create!({email: 'shessolucky@shesastar.com',
                          password: 'britneyspears',
                          password_confirmation: 'britneyspears',
-                         api_key: 'alsidjfioashefoij84'
-                       })
-    @body = ({origin: 'chicago,il',
-              destination: 'denver,co',
-              api_key: 'asidhfkiasdjf7249as'})
+                         api_key: 'iVpu4bGGDYYDtP2mD5b0Rgtt'
+                        })
+    body = ({origin: 'los angeles,ca',
+             destination: 'new york,ny',
+             api_key: user.api_key})
 
-    @headers = { 'CONTENT_TYPE' => 'application/json', "Accept": 'application/json' }
-  end
-  it 'can plan road trip with origin, destination, travel time and forecast' do
-    post '/api/v1/road_trip', headers: @headers, params: @body, as: :json
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept": 'application/json' }
+    post '/api/v1/road_trip', headers: headers, params: body, as: :json
 
     expect(response.status).to eq(200)
 
     results = JSON.parse(response.body, symbolize_names: true)[:data]
-    require "pry"; binding.pry
     expect(results).to be_a Hash
   end
 end
