5875f0ad99bb2a50b88baa6e87179cd3a96984af
sami-p <sami.peterson14@gmail.com>
2021-11-16 21:20:41 -0700

    Modify road trip info in itinerary method

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,6 +1,8 @@
 class RoadTripFacade
-  def self.itinerary(origin, destination)
-    # get directions from map service
-    
+  def self.itinerary(from, to)
+    location = MapFacade.location(to)
+    directions = MapService.directions(from, to)
+    eta_weather = WeatherFacade.all_hourly(location.latitude, location.longitude)
+    RoadTrip.new(from, to, directions, eta_weather)
   end
 end
