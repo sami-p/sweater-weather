76d15495d34eec7a020e73a3b318389a91a09df7
sami-p <sami.peterson14@gmail.com>
2021-11-16 17:33:20 -0700

    Test happy path for log in sessions

---
+++ spec/requests/api/v1/sessions_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Sessions Request' do
+  it 'logs in a user with email and password confirmation' do
+    user = User.create!({email: 'shessolucky@shesastar.com',
+                         password: 'britneyspears',
+                         password_confirmation: 'britneyspears',
+                         api_key: 'alsidjfioashefoij84'
+                       })
+
+    body = {email: 'shessolucky@shesastar.com',
+            password: 'britneyspears'}
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept": 'application/json' }
+
+    post '/api/v1/sessions', headers: headers, params: body, as: :json
+
+    results = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(results[:attributes][:email]).to eq(user.email)
+    expect(results[:attributes][:api_key]).to eq(user.api_key)
+  end
+end
