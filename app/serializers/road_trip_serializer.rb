b01f5b1508cb748a8b52f3aeb98e495a5f5dc609
sami-p <sami.peterson14@gmail.com>
2021-11-16 17:37:03 -0700

    Create mock road trip serializer

---
+++ app/serializers/road_trip_serializer.rb
@@ -0,0 +1,17 @@
+class RoadTripSerializer
+      {
+      data: {
+        id: null,
+        type: "roadtrip",
+        attributes: {
+        "start_cit": "Denver, CO",
+          end_city: Estes Park, CO,
+          travel_time: 2 hours, 13 minutes
+          weather_at_eta: {
+            temperature: 59.4,
+            conditions: partly cloudy with a chance of meatballs
+          }
+        }
+      }
+    }
+end
