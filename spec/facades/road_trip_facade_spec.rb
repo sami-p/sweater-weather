db498017b2aae7179448cb90e552c2af5122a7f6
sami-p <sami.peterson14@gmail.com>
2021-11-17 05:37:33 -0700

    Test road trip facade

--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -1,1 +1,15 @@
+require 'rails_helper'
 
+RSpec.describe 'Road Trip Facade' do
+  it 'can plan road trip with origin, destination, travel time & forecast', :vcr do
+    origin = 'los angeles, ca'
+    destination = 'new york, ny'
+
+    results = RoadTripFacade.itinerary(origin, destination)
+
+    expect(results.end_city).to eq(destination)
+    expect(results.start_city).to eq(origin)
+    expect(results.eta_weather.length).to eq(48)
+    expect(results.travel_time).to be_an Integer
+  end
+end
