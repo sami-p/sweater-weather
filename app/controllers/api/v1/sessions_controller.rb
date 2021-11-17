48505989520ee253a62fb6d0825325d4398a7cd4
sami-p <sami.peterson14@gmail.com>
2021-11-17 00:28:18 -0700

    Modify create action to include validation of email

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       render json: SessionsSerializer.session(user), status: 200
     else
       render :json => { :errors => "Invalid input" }, :status => 401
